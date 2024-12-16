834f2fc9bc2f768e4d5a978bb7dfa274405becbf
StuCat <stu.cat_023@icloud.com>
2024-12-16 17:34:21 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Register from './components/register'
+
+function App() {
+
+  return (
+    <>
+      <h1>Travel App</h1>
+      <p className="read-the-docs">
+        Start sharing your journey today!
+      </p>
+      <Register />
+    </>
+  )
+}
+
+export default App
