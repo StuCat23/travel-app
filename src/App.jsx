ec2c21cea3b76b8c987d709e3a776de945193cf3
StuCat <stu.cat_023@icloud.com>
2024-12-21 13:06:41 -0500

    Added places.json

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/navbar';
 import Hero from './components/Hero';
 import HomeCards from './components/HomeCards';
+import Places from './components/Places';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <HomeCards />
+      <Places />
     </>
   )
 }
